7c9a0eb04ee5bd95ad82989be83b1edba4bd5ee9
Abbas <divandriamir@gmail.com>
2025-09-21 10:51:14 +0330

    categories on the head section

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,4 +13,7 @@ class Category extends Model
     public function parent(){
         return $this->belongsTo(Category::class, 'parent_id');
     }
+    public function children(){
+        return $this->hasMany(Category::class, 'parent_id');
+    }
 }
